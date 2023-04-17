62e9924119512a41d6601c7ddcc1201beacd9e92
Janice <janiceralecha@gmail.com>
2023-04-17 16:37:55 -0500

    Add error messages in ApplicationController for unauthorized user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  # Ensuring that users are authorized to perform the actions they request, prevent unauthorized access.
+  before_action :authorize
+
+  def authorize
+    return render json: { error: "Not Authorized" }, status: :unauthorized unless session.include? :user_id
+  end
+
+  private
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
+  
+
+
 end
