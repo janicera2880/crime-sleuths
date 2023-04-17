f76d0f6decac7b5134c3a28b4b4b00f5e6125b94
Janice <janiceralecha@gmail.com>
2023-04-17 18:44:19 -0500

    Define 'get' and 'create' methods in ChannelController

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,4 +1,34 @@
 class ChannelsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-    #GET /channels
+    skip_before_action :authorize, only: [:index]
+
+# GET ALL /channels
+    def index
+        channel = Channel.all
+        render json: channel, include: ['posts']
+    end
+
+# POST create post
+    def create
+        channel = Channel.create!(channel_params)
+        render json: channel, status: :created
+    end
+
+# SHOW display a specific channel
+    def show
+        channel = Channel.find(params[:id])
+        render json: channel, include: ['posts', 'posts.user']
+    end
+  
+    private 
+
+    def channel_params
+        params.permit(:name, :description)
+    end
+
+    def render_not_found_response
+        render json: { error: "Channel Not Found" }, status: :not_found
+    end
+
 end
--- app/serializers/post_summary_serializer.rb
+++
@@ -1,8 +0,0 @@
-class PostSummarySerializer < ActiveModel::Serializer
-  attributes :summary
-
-  def summary
-    "#{self.object.title} - #{self.object.content[0..49]}..."
-  end
-
-end
