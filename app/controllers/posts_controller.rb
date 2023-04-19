4bed23a8316f702328a2b5d1e0fb8b26e4d5ee17
Janice <janiceralecha@gmail.com>
2023-04-19 07:01:19 -0500

    Create index method for all posts and include user.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    #GET /posts
+    def index
+        posts = Post.all.includes(:user)
+        render json: posts, include: :user
+      end
+    
+      
+
 end
