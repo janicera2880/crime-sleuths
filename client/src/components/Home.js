485983c7eba259c25a055ae6060606387ae285d9
Janice <janiceralecha@gmail.com>
2023-04-20 11:13:29 -0500

    Add new file home and login form.

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import headerImg from "../headerAnimate.gif"
+
+function Home() {
+    return (
+      <div className="logo">        
+        <img id="logoImg" src={headerImg} alt="crimesleuthsImg"/>
+      </div>
+    );
+  }
+  
+  export default Home;
