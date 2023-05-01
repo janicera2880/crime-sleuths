93a764a6786d420ff76400f977a96c613365bddd
Janice <janiceralecha@gmail.com>
2023-04-30 19:00:33 -0500

    Add comments to explain sections of code

--- client/src/Components/Blogger.js/Dashboard.js
+++ client/src/Components/Blogger.js/Dashboard.js
@@ -9,16 +9,20 @@ function Dashboard(){
     function DashboardView(){
         return(
         <div>
+            {/* Display the user's username */}
             {user.username} 's Dashboard
+            {/* Display the user's profile image */}
             <br />
             {user.image_url}
             <br />
            
             <br />
+            {/* Display the user's bio */}
             Bio:
             <br />
             {user.bio}
             <br />
+             {/* Display the user's location */}
             Location:
             <br />
             {user.location}
@@ -26,7 +30,6 @@ function Dashboard(){
             <Link to={"/user/posts"}>My Posts</Link>
             <br />
             <Link to={"/user/channels"}>My Channels</Link>
-            <br />
             <br />
             <Link to={"/updateuser"}><button>Edit Info</button></Link>
            
@@ -36,6 +39,7 @@ function Dashboard(){
 
     return(
         <div>
+            {/* If user is logged in, display DashboardView */}
             {user? <DashboardView /> : ""}
         </div>
     )
