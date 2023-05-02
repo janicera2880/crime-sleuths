2230b4f2aaac9c5a48eed22e18c9805c10e74323
Janice <janiceralecha@gmail.com>
2023-05-02 11:37:39 -0500

    Refactor App.js using latest React and fix context dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import Home from './Components/Home';
 import './App.css';
 import { UserContext } from './Context/UserContext';
-import {useContext, useEffect} from "react";
-
+import {useContext, useEffect, useState} from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import Home from './Components/Home';
+import SignupForm from "./Components/Blogger/SignupForm";
+import LoginForm from "./Components/Blogger/LoginForm";
+import Dashboard from "./Components/Blogger/Dashboard";
+import EditDashboard from "./Components/Blogger/EditDashboard";
+import ChannelsLists from "./Components/Thread/ChannelsLists";
+import ChannelContainer from "./Components/Thread/ChannelContainer";
+import UserPostDetails from "./Components/Thread/UserPostDetails";
+import PostLists from './Components/Thread/PostLists';
 
 
 
 function App() {
-  const { setUser } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+  const { setPosts, setUserPosts } = useContext(PostsContext);
+  const [channels, setChannels] = useState([]);
+
 
   useEffect( ()=>{
     fetch("/me").then( (r) => {
@@ -15,15 +27,49 @@ function App() {
             r.json().then( (data) => setUser(data) );
         }
     });
-  }, [setUser] )
+  }, []);
+
+  useEffect( ()=>{
+    if (user != null){
+      fetch(`/users/${user.id}/posts`).then(r=>r.json()).then(data=>{
+        setUserPosts(data);
+      })
+    }
+  }, [user]);
+
+  useEffect( ()=>{
+    fetch("/channels").then( r => r.json() ).then( (data)=>{
+        setChannels(data);
+    })
+  }, []);
+
+  function handleAddChannel(newChannel) {
+    setChannels([newChannel, ...channels]);
+  }
   
   
   return (
-    <div>
-       
-      <Home />
-     
-    </div>
+    
+    <BrowserRouter>
+      <div className="App">
+
+        <NavBar />
+        <Routes>
+          {/* Real routes begin here */}
+          <Route path="/user/channels" element={<UserChannelPage />} />
+          <Route path="/user/posts" element={<PostLists />} />
+          <Route path="/posts/:id" element={<UserPostDetails />} />
+          <Route path="/channels/:id" element={<ChannelContainer />} />
+          <Route path="/channels" element={<ChannelsLists />} />
+          <Route path="/updatedashboard" element={<EditDashboard />} />
+          <Route path="/user" element={<Dashboard />} />
+          <Route path="/signup" element={<SignupForm />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
