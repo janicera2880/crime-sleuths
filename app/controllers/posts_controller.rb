45624e71158bbb9ad284c8ad9b85b9b330e106dc
Janice <janiceralecha@gmail.com>
2023-05-07 12:44:02 -0500

    Modify PostsController, change response for update to ok from created since it's an update operation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,41 +1,44 @@
 class PostsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    before_action :authorize
+    skip_before_action :authorize, only: [:index, :show]
     
-    
+    # GET
     def index
-        if params[:channel_id]
-            channel = Channel.find(params[:channel_id])
-            posts = channel.posts
+        if params[:user_id]
+            user = find_user
+            render json: user.posts.all, status: :ok
         else 
-            render json: render_not_found_response
+            render json: Post.all, status: :ok
         end
-        render json: posts
     end
       
     # GET /posts/:id
     def show
-        post = Post.find(params[:id])
-        render json: post
+        post = find_post
+        render json: post, status: :ok
     end
 
     # POST
     def create
-        post = @current_user.posts.create!(post_params)
-        render json: post, status: :created 
+        user = find_user
+        new_post = user.posts.create!(post_params)
+        render json: new_post, status: :created 
+    rescue ActiveRecord::RecordInvalid => e
+        render json: {errors: e.record.errors}, status: :unprocessable_entity
     end
 
+    # PATCH
     def update
         post = find_post
-        if  post.user === @current_user
-            post.update!(post_params)
-            render json: post, status: :created 
-        else render json: {error: "User is not valid"}
-        end
+        post.update!(post_params)
+        render json: post, status: :ok
     end
 
+    # DELETE
     def destroy
         post = find_post
-        post.destroy
+        post.destroy!
         head :no_content
     end
 
@@ -48,5 +51,17 @@ class PostsController < ApplicationController
     def find_post 
         Post.find(params[:id])
     end 
+
+    def find_user
+        User.find(params[:user_id])
+    end
+
+    def authorize
+        render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
+    end
+
+    def render_not_found_response
+        render json: {error: "Record not found"}, status: :not_found
+    end
 
 end
