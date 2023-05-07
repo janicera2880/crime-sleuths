925d9a9e3173a26b5e812adb1a37d68f7c28b08f
Janice <janiceralecha@gmail.com>
2023-05-07 12:15:20 -0500

    Add find_channel method to find the channel based on the provided id parameter.

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,36 +1,37 @@
 class ChannelsController < ApplicationController
-    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-    skip_before_action :authorize, only: :index
+  skip_before_action :authorize, only: [:index]
 
-# GET /channels
-    def index
-        channel = Channel.all.order(:id) 
-        render json: channel, include: ['posts', 'posts.user']
-    end
+  # GET /channels
+  def index
+    channels = Channel.all.order(:id)
+    render json: channels, include: ['posts', 'posts.user']
+  end
 
-    # SHOW display a specific /channels/:id
-    def show
-        channel = Channel.find(params[:id])
-        render json: channel, include: ['posts', 'posts.user']
-    end
+  # SHOW display a specific /channels/:id
+  def show
+    channel = find_channel
+    render json: channel, include: ['posts', 'posts.user']
+  end
 
-# POST create Channel
-    def create
-        channel = Game.create!(channel_params)
-        render json: channel, status: :created
-    end
+  # POST create Channel
+  def create
+    channel = Channel.create!(channel_params)
+    render json: channel, status: :created
+  end
 
+  private
 
-  
-    private 
+  def channel_params
+    params.permit(:name, :description)
+  end
 
-    def channel_params
-        params.permit(:name, :description)
-    end
+  def find_channel
+    Channel.find(params[:id])
+  end
 
-    def render_not_found_response
-        render json: { error: "Channel Not Found" }, status: :not_found
-    end
-
+  def render_not_found_response
+    render json: { error: 'Channel Not Found' }, status: :not_found
+  end
 end
