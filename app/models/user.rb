b2151b3e962968526d42535e93083c24befa1048
Janice <janiceralecha@gmail.com>
2023-05-07 10:44:26 -0500

    UserPostSerializer is specified as the serializer for the posts association in ChannelSerializer.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
     # The 'has_many :posts' association indicates that a user has multiple posts.
     # The 'has_many :channels, through: :posts' association allows a user to access channels through their posts. 
-    has_many :posts, dependent: :destroy
+    has_many :posts
     has_many :channels, through: :posts
 
     # Method to store password encrypted using bcrypt and stored in the database as a password digest.
     has_secure_password
 
+    #Validations
     validates :username, presence: true, uniqueness: true, length: { minimum: 6 }
     validates :password, length: { in: 8..20 }
     validates :image_url, presence: true
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :image_url, :bio, :location
 
   
-  has_many :channels
-  has_many :posts, serializer: UserPostSerializer
+  has_many :posts
+  has_many :channels, through: :posts
+
 end
